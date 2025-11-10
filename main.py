6c3e797d775ec1b4f2c2a532824bd9f5c47d8d1c
nut <farhaliyabrar06@gmail.com>
2025-11-10 08:19:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ import os
 
 def banner():
     print("""
-     __  ____     ___  __   _  _  __ _  ____
-    (  )(  _ \   / __)/  \ / )( \(  ( \(_  _)
-     )(  ) __/  ( (__(  O )) \/ (/    /  )(  
-    (__)(__)     \___)\__/ \____/\_)__) (__) 
+     __  ____     ___  __   _  _  __ _  ____  
+    (  )(  _ \   / __)/  \ / )( \(  ( \(_  _) 
+     )(  ) __/  ( (__(  O )) \/ (/    /  )(    
+    (__)(__)     \___)\__/ \____/\_)__) (__)   
     
     """)
 
 def menu():
     os.system('cls')
     banner()
-    print("Pilih opsi:")
-    print("1. Hitung IP B")
-    print("2. Hitung IP C")
-    choice = input("Masukkan pilihan (1 atau 2): ")
+    print("Select an option:")
+    print("1. Calculate IP Class B")
+    print("2. Calculate IP Class C")
+    choice = input("Enter your choice (1 or 2): ")
     if choice == '1':
         hitung_ip_b.calculate_b()
     elif choice == '2':
         hitung_ip_c.calculate_c()
     else:
-        print("Pilihan tidak valid. Coba lagi.")
+        print("Invalid choice. Please try again.")
         menu()
 
 if __name__ == "__main__":
